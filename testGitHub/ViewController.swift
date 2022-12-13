7230da5db70452c1f490d5e304e563fd56d1adb0
MaxShikin <maxshikin@gmail.com>
2022-12-13 16:22:26 +0400

    Initial Commit

---
+++ testGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGitHub
+//
+//  Created by maxshikin on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
